074e37f8395d57b9fad48768a3fd7da838417e55
Lubov May <liubov.mayorova@gmail.com>
2021-10-08 20:13:12 -0700

    Installed gh-pages package as a dev-dependency of the React app. (To be able to host app on github) Added the "homepage" property to package.json file. In the package.json file, added the predeploy property and the deploy property with the existing scripts property.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,26 @@
+import { saveQuestionAnswer } from '../utils/api'
+
 export const RECEIVE_QUESTIONS = 'GET_QUESTIONS'
+export const SAVE_QUESTIONS_ANSWER = 'SAVE_QUESTIONS_ANSWER'
+
+export function saveAnswer(vote) {
+    return {
+        type: SAVE_QUESTIONS_ANSWER,
+        vote,
+    }
+}
+
+export function handleSaveAnswer(qid, vote) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState()
+        return saveQuestionAnswer({
+            vote,
+            authedUser,
+            qid,
+        })
+            .then((vote) => dispatch(saveAnswer(vote)))
+    }
+}
 
 export function receiveQuestions(questions) {
     return {
