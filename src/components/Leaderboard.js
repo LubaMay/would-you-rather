5d388edce31185a741060c6d589062b69f76fd8b
Lubov May <liubov.mayorova@gmail.com>
2021-10-14 17:48:02 -0700

    Updated UI and CSS for Leaderboard.js Component.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,49 +2,58 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { formatQuestion } from "../utils/_DATA";
 
-class LeaderBoard extends Component {
+class Leaderboard extends Component {
   render() {
-    const { user } = this.props;
-    const { name, avatarURL, answers, questions } = user;
-
-    const answeredQs = Object.keys(answers).length;
-    const createdQs = questions.length;
-    const score = answeredQs + createdQs;
+    const { usersList } = this.props;
 
     return (
-      <div>
-        <div className="question">
-          <div className="leaderboard-grid">
-            <div>
-              <img
-                src={avatarURL}
-                alt={`Avatar of ${name}`}
-                className="avatar"
-              />
-            </div>
+      <div className="question leaderboard">
+        {usersList.map((user, id) => {
+          const { name, avatarURL, answers, questions } = user;
 
-            <div>
-              <h3>{name}</h3>
-              <p>Answered Questions: {answeredQs}</p>
-              <p>Created Questions: {createdQs}</p>
-            </div>
+          const answeredQs = Object.keys(answers).length;
+          const createdQs = questions.length;
+          const score = answeredQs + createdQs;
 
-            <div className="score">
-              <span>{score}</span>
+          return (
+            <div className="board" key={id}>
+              <div className="leaderboard-grid">
+                <div className="leaderboard-img">
+                  <img
+                    src={avatarURL}
+                    alt={`Avatar of ${name}`}
+                    className="avatar"
+                  />
+                </div>
+
+                <div className="leaderboard-info">
+                  <h3>{name}</h3>
+                  <p>Answered Questions: {answeredQs}</p>
+                  <hr />
+                  <p>Created Questions: {createdQs}</p>
+                </div>
+
+                <div className="score">
+                  <p>Score</p>
+                  <p>
+                    <span className="score-shape">{score}</span>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-function mapStateToProps({ authedUser, users, questions }) {
-  const user = users[authedUser];
+function mapStateToProps({ users }) {
+  const usersList = Object.values(users);
 
   return {
-    user,
+    usersList,
   };
 }
 
-export default connect(mapStateToProps)(LeaderBoard);
+export default connect(mapStateToProps)(Leaderboard);
